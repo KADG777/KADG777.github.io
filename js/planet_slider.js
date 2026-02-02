16b3238940e94d991ee9aa7f50d361881df50bf1
KADG777 <hcz2006@qq.com>
2026-02-02 13:16:56 +0800

    Site updated: 2026-02-02 13:16:54

--- js/planet_slider.js
+++ js/planet_slider.js
@@ -13,35 +13,32 @@ document.addEventListener('DOMContentLoaded', function() {
   container.innerHTML = `
     <div class="planet-wrapper">
       <a href="/music/" class="planet-item" title="歌单">
-        <div class="planet music-planet">
-          <div class="planet-ring"></div>
+        <div class="planet-box">
+          <img src="/img/planets/music.png" class="planet-img" onerror="this.onerror=null;this.src='/img/loading.gif';">
         </div>
         <span>歌单</span>
       </a>
       <a href="/archives/" class="planet-item" title="归档">
-        <div class="planet archive-planet">
-          <div class="crater c1"></div>
-          <div class="crater c2"></div>
+        <div class="planet-box">
+          <img src="/img/planets/archives.png" class="planet-img" onerror="this.onerror=null;this.src='/img/loading.gif';">
         </div>
         <span>归档</span>
       </a>
       <a href="/profile/" class="planet-item" title="简介">
-        <div class="planet profile-planet">
-          <div class="land l1"></div>
-          <div class="land l2"></div>
+        <div class="planet-box">
+          <img src="/img/planets/profile.png" class="planet-img" onerror="this.onerror=null;this.src='/img/loading.gif';">
         </div>
         <span>简介</span>
       </a>
       <a href="/tags/" class="planet-item" title="标签">
-        <div class="planet tag-planet">
-           <div class="stripe s1"></div>
-           <div class="stripe s2"></div>
+        <div class="planet-box">
+          <img src="/img/planets/tags.png" class="planet-img" onerror="this.onerror=null;this.src='/img/loading.gif';">
         </div>
         <span>标签</span>
       </a>
       <a href="/categories/" class="planet-item" title="分类">
-        <div class="planet category-planet">
-          <div class="spot sp1"></div>
+        <div class="planet-box">
+          <img src="/img/planets/categories.png" class="planet-img" onerror="this.onerror=null;this.src='/img/loading.gif';">
         </div>
         <span>分类</span>
       </a>
@@ -49,5 +46,15 @@ document.addEventListener('DOMContentLoaded', function() {
   `;
 
   // Insert before the recent posts section
-  target.parentNode.insertBefore(container, target);
+  var target = document.getElementById('recent-posts');
+  if (target) {
+    target.parentNode.insertBefore(container, target);
+  } else {
+    // Fallback: try to find the first child of the main layout content column
+    // The main content column is usually .layout > div:first-child
+    var layoutContent = document.querySelector('.layout > div:first-child');
+    if (layoutContent) {
+      layoutContent.insertBefore(container, layoutContent.firstChild);
+    }
+  }
 });
